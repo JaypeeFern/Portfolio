8d526c49aa1274af9944e9804ae30a4877b97dd2
John Paul Fernandez <103358200+JaypeeFern@users.noreply.github.com>
2023-09-03 15:26:29 +0800

    added light and dark mode buttons

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,7 +1,22 @@
+import { Button } from "@/components/ui/button"
+import { Sun, Moon } from "lucide-react"
+import { useTheme } from "@/components/ThemeProvider"
+
 export default function Homepage() {
+    const { theme, setTheme } = useTheme()
+
+
     return (
-        <div className="bg-slate-100">
-            <span>homepage</span>
+        <div className="h-screen">
+            {theme == 'light' ?
+                (<Button variant='outline' size='icon' onClick={() => setTheme('dark')}>
+                    <Moon />
+                </Button>)
+                :
+                (<Button variant='outline' size='icon' onClick={() => setTheme('light')}>
+                    <Sun />
+                </Button>)
+            }
         </div>
     )
 }
