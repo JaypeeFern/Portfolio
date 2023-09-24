c964f0779e186ad2fff47bd31394b0325e2f3473
John Paul Fernandez <103358200+JaypeeFern@users.noreply.github.com>
2023-09-24 20:59:21 +0800

    minor css addition

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Projects() {
     return (
         <>
-            <div id="project-section" className="projects-container h-full flex" >
+            <div id="project-section" className="projects-container h-full flex mb-5">
                 <span>Projects</span>
             </div>
         </>
