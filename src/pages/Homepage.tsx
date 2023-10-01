a0149d5e6a6339750ac972e52af4bd23964f656b
John Paul Fernandez <103358200+JaypeeFern@users.noreply.github.com>
2023-10-01 15:25:22 +0800

    [mock] added projects styling

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button"
 export default function Homepage() {
     return (
         <>
-            <div id="home-section" className="home-container h-full flex mb-5">
+            <main id="home-section" className="home-container h-full flex mb-5">
                 <motion.div initial={{ x: -1000 }} animate={{ x: 0 }} transition={{ type: 'spring', from: -500, duration: 1.25, delay: 0.5 }} className="h-full flex flex-col justify-center p-14">
                     <div className="flex flex-col gap-3">
                         <span className="text-7xl font-extrabold">Hi!</span>
@@ -15,7 +15,7 @@ export default function Homepage() {
                         Contact Me!
                     </Button>
                 </motion.div>
-            </div >
+            </main >
         </>
     )
 }
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,11 +1,62 @@
-import React from 'react'
+import { faker } from '@faker-js/faker'
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+import { Separator } from '@/components/ui/separator'
+
+interface Data {
+    projectName: string
+    projectDescription: string
+    projectSampleImage: string
+    projectDemoLink: string
+    projectClient: [{
+        name: string,
+        duration: string
+    }]
+}
 
 export default function Projects() {
+
+    let projects: Data[] = []
+
+    const generateRandomProjects = (index?: number): Data => {
+        return {
+            projectName: `Project ${index}`,
+            projectDescription: faker.lorem.paragraph(10),
+            projectSampleImage: faker.image.url(),
+            projectDemoLink: faker.internet.url(),
+            projectClient: [{
+                name: faker.company.name(),
+                duration: 'Present'
+            }]
+        }
+    }
+
+    for (let i = 0; i < 6; i++) {
+        projects.push(generateRandomProjects(i + 1))
+    }
+
     return (
-        <>
-            <div id="project-section" className="projects-container h-full flex mb-5">
-                <span>Projects</span>
+        <main className='projects-container flex h-full p-4 gap-10'>
+            <div className='grid grid-cols-3 gap-3'>
+                {projects.map((project, index) => (
+                    <Card key={index}>
+                        <CardHeader className='max-h-[170px]'>
+                            <CardTitle>{project.projectName}</CardTitle>
+                            <Separator className='h-[3px]' />
+                            {project.projectClient.map((client, index) => (
+                                <span key={index} className='font-thin'>{client.name} | {client.duration}</span>
+                            ))}
+                            <CardDescription className='line-clamp-2'>{project.projectDescription}</CardDescription>
+                        </CardHeader>
+                        <CardContent className='mb-auto mt-auto'>
+                            <img src={project.projectSampleImage} className='rounded-md' />
+                        </CardContent>
+                        <CardFooter className="flex flex-col">
+                            <Button variant='outline'>Demo</Button>
+                        </CardFooter>
+                    </Card>
+                ))}
             </div>
-        </>
+        </main>
     )
 }
